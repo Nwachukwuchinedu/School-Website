965e748bb1923946d42c3c723eae0de682ccd742
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-01-29 11:24:16 +0100

    added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          600: "rgba(184, 60, 31, 0.6)",
+          700: "rgba(184, 60, 31, 0.7)",
+          800: "rgba(184, 60, 31, 0.8)",
+          900: "rgba(184, 60, 31, 0.9)",
+          1000: "rgba(184, 60, 31, 1)",
+        },
+        green: {
+          600: "rgba(3, 100, 3, 0.6)",
+          700: "rgba(3, 100, 3, 0.7)",
+          800: "rgba(3, 100, 3, 0.8)",
+          900: "rgba(3, 100, 3, 0.9)",
+          1000: "rgba(3, 100, 3, 1)",
+        },
+      },
+    },
   },
   plugins: [],
 };
